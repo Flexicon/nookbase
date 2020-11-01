89aacfb72a40e43b3f660015c5308a1a9e1bbd55
Michal Repec <mikerepec@gmail.com>
2020-11-01 14:00:55 +0100

    Setup viper and env vars

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	cloud.google.com/go v0.70.0 // indirect
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/pkg/errors v0.9.1
+	github.com/spf13/viper v1.7.1
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	google.golang.org/api v0.33.0
 	google.golang.org/appengine v1.6.7 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
@@ -21,7 +25,11 @@ var (
 )
 
 func main() {
-	service, err := sheets.NewService(context.TODO(), option.WithCredentialsFile("./creds.json"))
+	if err := initViper(); err != nil {
+		log.Fatal(err)
+	}
+
+	service, err := sheets.NewService(context.TODO(), option.WithCredentialsJSON(getCredsJSON()))
 	if err != nil {
 		log.Fatalf("failed to setup sheets service: %v", err)
 	}
@@ -39,5 +47,49 @@ func main() {
 	e.GET("/search/:category", SearchHandler(service))
 	e.GET("/seasonal/:hemisphere/:category", SeasonalHandler(service))
 
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", viper.GetInt("port"))))
+}
+
+func initViper() error {
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	// Prepare for Environment variables
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	// Defaults
+	viper.SetDefault("port", 3000)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			// Config file was found but another error was produced
+			return errors.Wrap(err, "viper error")
+		}
+	}
+
+	return nil
+}
+
+func getCredsJSON() []byte {
+	privateKey := strings.ReplaceAll(viper.GetString("google.private_key"), "\n", "\\n")
+	raw := fmt.Sprintf(`{
+		"type": "service_account",
+		"project_id": "cohesive-armor-129523",
+		"private_key_id": "%s",
+		"private_key": "%s",
+		"client_email": "%s",
+		"client_id": "%s",
+		"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+		"token_uri": "https://oauth2.googleapis.com/token",
+		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+		"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/animal-crossing-sheets%%%%40cohesive-armor-129523.iam.gserviceaccount.com"
+	  }`,
+		viper.GetString("google.private_key_id"),
+		privateKey,
+		viper.GetString("google.client_email"),
+		viper.GetString("google.client_id"),
+	)
+
+	return []byte(raw)
 }
